8f1429c66dc7fb204a1f11b97575a762d33862f9
Macrow <Macrow_wh@163.com>
2011-06-07 22:52:34 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc1'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'json'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+# haml
+gem 'haml'
+gem 'haml-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yobi::Application.config.secret_token = 'dd30fda399398e3b59b7bc8edde4010785d9d1705c7cc4206f830c4a7ce44e6d5f5f558d678c13e5c9ab03fd9873aec7e433dd4187c5339e0a3dca5b2f7ad69d'
