31d25c6b91dfd38d36e8235d40f08368abcb18d2
Macrow <Macrow_wh@163.com>
2011-09-13 23:09:13 +0800

    update sidebar and slide pictures

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -38,7 +38,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [120, 120]
+    process :resize_to_fit => [120, 120]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
