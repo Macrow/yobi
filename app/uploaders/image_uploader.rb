c179a370a70a305cfe444548aaa351adce24426c
Macrow <Macrow_wh@163.com>
2011-10-04 11:12:56 +0800

    fixed image store dir bug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,7 +15,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/product_images/#{Time.now.strftime("%Y-%m")}"
+    # "uploads/product_images/#{Time.now.strftime("%Y-%m")}"
+    "uploads/product_images"
   end
 
   def cache_dir
